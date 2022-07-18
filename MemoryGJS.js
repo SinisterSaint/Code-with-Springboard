2b61617185946b17e1ab3fb46b6ab048a42e1f3c
Saphanley Saint-Vil <108836179+SinisterSaint@users.noreply.github.com>
2022-07-18 17:58:39 -0400

    Update MemoryGJS.js

--- MemoryGJS.js
+++ MemoryGJS.js
@@ -45,22 +45,13 @@ function createDivsForColors(colorArray) {
   for (let color of colorArray) {
     // create a new div
     const newDiv = document.createElement("div");
-    
     // const onclick = document.createEvent(onclick)
-    
-    
     newDiv.classList.add("backOfCard")
-    
-
     // give it a class attribute for the value we are looping over
     newDiv.classList.add(color);
-    
-
     // call a function handleCardClick when a div is clicked on
     newDiv.addEventListener("click", handleCardClick);
-    newDiv.addEventListener("click", flipcard)
-
-    // append the div to the element with an id of game
+     // append the div to the element with an id of game
     gameContainer.append(newDiv);
   }
 }
@@ -68,14 +59,18 @@ function createDivsForColors(colorArray) {
 // TODO: Implement this function!
 function handleCardClick(event) {
   // you can use event.target to see which element was clicked
+flipcard(event.target) 
   console.log("you just clicked", event.target);
 }
 
 // Function for when card is clicked, it removes the backOfCard class
-function flipcard() {
+function flipcard(e) {
   // Element.classList.remove("backOfCard")
   const pickedCard = e.currentTarget;
-  pickedCard.className = target.className.replace("backOfCard", "")
+  e.target.classList.toggle("backOfCard");
+  
+
+  // pickedCard.className = element.classList.remove("backOfCard");
 
 }
 
